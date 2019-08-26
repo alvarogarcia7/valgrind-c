9715b53d9ee142f388a33154595bdd307bdac3c1
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-08-26 23:31:52 +0200

    Remove old comments

--- examples/sample-memory-leak/valgrind-1.c
+++ examples/sample-memory-leak/valgrind-1.c
@@ -3,9 +3,9 @@
 void f(void)
 {
    int *x = malloc(10 * sizeof(int));
-   x[9] = 0; // problem 1: heap block overrun
+   x[9] = 0;
    free(x);
-} // problem 2: memory leak -- x not freed
+}
 
 int main(void)
 {
